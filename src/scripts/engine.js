8e642ab6528da624a73d38cea1f06c9ddf079ccf
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-01-02 15:55:06 -0300

    added sound effect :sparkles:

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -101,7 +101,7 @@ async function setCardsField(cardId) {
 }
 
 async function drawButton(text) {
-  state.actions.button.innerText = text;
+  state.actions.button.innerText = text.toUpperCase();
   state.actions.button.style.display = "block";
 }
 async function updateScore() {
@@ -109,17 +109,20 @@ async function updateScore() {
 }
 
 async function checkDuelResults(playerCardId, computerCardId) {
-  let duelResults = "Empate";
+  let duelResults = "draw";
   let playerCard = cardData[playerCardId];
 
   if (playerCard.WindOf.includes(computerCardId)) {
-    duelResults = "Ganhou";
+    duelResults = "win";
+
     state.score.playerScore++;
   }
   if (playerCard.LoseOf.includes(computerCardId)) {
-    duelResults = "Perdeu";
+    duelResults = "lose";
+
     state.score.playerScore++;
   }
+  await playAudio(duelResults);
   return duelResults;
 }
 async function removeAllCardsImages() {
@@ -147,6 +150,23 @@ async function drawCards(cardNumbers, fieldSide) {
 
     document.getElementById(fieldSide).appendChild(cardImage);
   }
+}
+
+async function resetDuel() {
+  state.cardSprites.avatar.src = "";
+  state.actions.button.style.display = "none";
+
+  state.fieldCards.player.style.display = "none";
+  state.fieldCards.computer.style.display = "none";
+  init();
+}
+
+async function playAudio(status) {
+  const audio = new Audio(`./src/assets/audios/${status}.wav`);
+
+  try {
+    audio.play();
+  } catch (error) {}
 }
 
 function init() {
